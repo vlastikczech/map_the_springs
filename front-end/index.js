f0dca09bf2a99f81a7cbcaddefd7108d3e2aaab1
vlastik <vlastikczech1@gmail.com>
2019-12-08 19:30:49 -0700

    Updated popup overlay; Changed the map layer

--- front-end/index.js
+++ front-end/index.js
@@ -1,20 +1,14 @@
 import 'ol/ol.css';
-import Feature from 'ol/Feature';
-import OSM from 'ol/source/OSM';
 import Map from 'ol/Map';
 import Overlay from 'ol/Overlay';
 import View from 'ol/View';
 import {toStringHDMS} from 'ol/coordinate';
 import {toLonLat} from 'ol/proj';
-import Point from 'ol/geom/Point';
 import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
 import {fromLonLat} from 'ol/proj';
-import TileJSON from 'ol/source/TileJSON';
 import VectorSource from 'ol/source/Vector';
-import {Icon, Style} from 'ol/style';
 import GeoJSON from 'ol/format/GeoJSON';
-import { listenerCount } from 'cluster';
-import { totalmem } from 'os';
+import XYZ from 'ol/source/XYZ';
 
 let url = 'http://127.0.0.1:5000/api/v1/resources/json'
 fetch(url).then(function(response) {
@@ -24,11 +18,10 @@ fetch(url).then(function(response) {
       fetch('http://127.0.0.1:5000/api/v1/resources/geojson').then(function(response) {
       return response.json();
     }).then(function(data) {
-        var key = 'pk.eyJ1Ijoidmxhc3Rpa2N6ZWNoIiwiYSI6ImNrM3dmbHd6dzAxY3Qza3FtbTBjenc2ZGwifQ.En3JITruEXqtp8NCqxM-gA';
 
         /**
          * Elements that make up the popup.
-         */
+        */
         var container = document.getElementById('popup');
         var content = document.getElementById('popup-content');
         var closer = document.getElementById('popup-closer');
@@ -38,7 +31,7 @@ fetch(url).then(function(response) {
 
         /**
          * Create an overlay to anchor the popup to the map.
-         */
+        */
         var overlay = new Overlay({
           element: container,
           autoPan: true,
@@ -51,7 +44,7 @@ fetch(url).then(function(response) {
         /**
          * Add a click handler to hide the popup.
          * @return {boolean} Don't follow the href.
-         */
+        */
         closer.onclick = function() {
           overlay.setPosition(undefined);
           closer.blur();
@@ -59,17 +52,20 @@ fetch(url).then(function(response) {
         };
 
         var vectorSource = new VectorSource({
-        features: (new GeoJSON()).readFeatures(geojsonObject)
+          features: (new GeoJSON()).readFeatures(geojsonObject)
         });
     
         var vectorLayer = new VectorLayer({
-        source: vectorSource
+          source: vectorSource
         });
         
         var rasterLayer = new TileLayer({
-          source: new TileJSON({
-            url: 'https://api.tiles.mapbox.com/v4/mapbox.natural-earth-hypso-bathy.json?access_token=' + key,
-            crossOrigin: 'anonymous'
+          source: new XYZ({
+            attributions: ['Powered by Esri',
+                           'Source: Esri, DigitalGlobe, GeoEye, Earthstar Geographics, CNES/Airbus DS, USDA, USGS, AeroGRID, IGN, and the GIS User Community'],
+            attributionsCollapsible: false,
+            url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+            maxZoom: 23
           })
         })
           
@@ -91,6 +87,7 @@ fetch(url).then(function(response) {
           function(feature) {
             return feature;
           });
+
         var coordinate = evt.coordinate;
         var hdms = toStringHDMS(toLonLat(coordinate));
         var title, url;
@@ -101,11 +98,14 @@ fetch(url).then(function(response) {
           title = null;
           url = null;
         }
-        
+
         if(title !== null) {
-          content.innerHTML = '<h4>' + title +
-          '</h4><a href='+ url +'>' + url + '</a>' +
-          '</br><code>' + hdms + '</code>';
+          content.innerHTML = '<div class="card text-center" style="width: 18rem;">' +
+            '<div class="card-body">' + 
+                '<h5 class="card-title">' + title + '<h5/>' +
+                '<a href="' + url + '" class="btn btn-primary">More Info</a>' +
+            '</div>' +
+          '</div>';
           overlay.setPosition(coordinate);
         }
       });
